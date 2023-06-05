736c9072b6f0e275591472ba734ea3d3f9318e01
k-hye.yun <khye12.yun@gmail.com>
2023-06-05 14:59:49 +0900

    base code
    Signed-off-by: k-hye.yun <khye12.yun@gmail.com>

--- DeviceDriverTest/test.cpp
+++ DeviceDriverTest/test.cpp
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include "../DeviceDriver/DeviceDriver.cpp/"
+
+using namespace testing;
+using namespace std;
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
 }
