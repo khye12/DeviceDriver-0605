bad8e68d43e5390b31aebab1721571b06a96ca09
k-hye.yun <khye12.yun@gmail.com>
2023-06-05 15:46:13 +0900

    read feature

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,12 +1,21 @@
 ﻿#include "DeviceDriver.h"
 
+#include <exception>
+
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int firstReadTryValue= (int)(m_hardware->read(address));
+    for(int tryCount = 2; tryCount <= 5; tryCount++)
+    {
+        int nextReadTryValue = (int)(m_hardware->read(address));
+        if (firstReadTryValue == nextReadTryValue) continue;
+        throw std::exception("Exception!!");
+    }
+    return firstReadTryValue;
 }
 
 void DeviceDriver::write(long address, int data)
--- DeviceDriverTest/test.cpp
+++ DeviceDriverTest/test.cpp
@@ -1,11 +1,41 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include "../DeviceDriver/DeviceDriver.cpp/"
+#include "../DeviceDriver/DeviceDriver.cpp"
 
 using namespace testing;
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+class MockDevice : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char), (override));
+};
+
+TEST(TestCaseName, TestNameFiveRead) {
+	
+	MockDevice mock_device;
+
+	EXPECT_CALL(mock_device, read)
+		.Times(5);
+
+	DeviceDriver driver(&mock_device);
+	driver.read(0xA);
+		
+}
+
+TEST(TestCaseName, Exception) {
+
+	MockDevice mock_device;
+
+	EXPECT_CALL(mock_device, read(0x2))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x3));
+
+	DeviceDriver driver(&mock_device);
+	EXPECT_THROW(driver.read(0x2), std::exception);
 
 }
